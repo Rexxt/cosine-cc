debcabe79d2c76c19bdf5198a042b120adfc7770
Mizu <40831463+Rexxt@users.noreply.github.com>
2022-12-09 16:51:06 +0100

    added functions to remove a user and change passwords

--- cos-api/cos.lua
+++ cos-api/cos.lua
@@ -1,7 +1,7 @@
 -- Cosine API
 -- Cosine is a small multi-accounting and permissions manager for ComputerCraft.
 
-local sha256 = require "sha256"
+local sha256 = require "cos-api.sha256"
 local function deepcopy(t)
     local t2 = {}
     for k, v in t do
@@ -63,14 +63,14 @@ function cos.login(username, password)
     if password == nil then
         error('missing password', 2)
     end
-    if users[username] == nil then
+    if cos[cos_idx].users[username] == nil then
         error('did not find user', 2)
     end
-    if sha256(password) ~= users[username].password then
+    if sha256(password) ~= cos[cos_idx].users[username].password then
         error('wrong password', 2)
     end
     cos[cos_idx].connected_user = username
-    settings.set("cos_users", cos[cos_idx].users)
+    cos[cos_idx].apis.settings.set("cos_users", cos[cos_idx].users)
     return true
 end
 
@@ -80,10 +80,20 @@ end
 
 function cos.getPermissions()
     local user = cos[cos_idx].connected_user
+    if user == nil then
+        return nil
+    end
     return cos[cos_idx].users[user].permissions
 end
 
 function cos.createUser(username, password)
+    local perms = cos.getPermissions()
+    if perms == nil then
+        error('not logged in', 2)
+    end
+    if not perms.manage_users then
+        error('missing permission "manage_users" for user '..cos.getUsername(), 2)
+    end
     cos[cos_idx].users[username] = {
         password = sha256(password),
         permissions = {
@@ -103,6 +113,37 @@ function cos.createUser(username, password)
             manage_users = false
         }
     }
-    settings.set("cos_users", cos[cos_idx].users)
+    cos[cos_idx].apis.settings.set("cos_users", cos[cos_idx].users)
+    return true
+end
+
+function cos.removeUser(username)
+    local perms = cos.getPermissions()
+    if perms == nil then
+        error('not logged in', 2)
+    end
+    if not perms.manage_users then
+        error('missing permission "manage_users" for user '..cos.getUsername(), 2)
+    end
+    cos[cos_idx].users[username] = nil
+    cos[cos_idx].apis.settings.set("cos_users", cos[cos_idx].users)
+    return true
+end
+
+function cos.changePassword(username, old_password, new_password)
+    local perms = cos.getPermissions()
+    if perms == nil then
+        error('not logged in', 2)
+    end
+    if cos.getUsername() ~= username and not perms.manage_users then
+        error('missing permission "manage_users" for user '..cos.getUsername(), 2)
+    end
+    if sha256(old_password) ~= cos[cos_idx].users[username].password then
+        error('wrong initial password', 2)
+    end
+    if old_password == new_password then
+        error('cannot change to the password already in use', 2)
+    end
+    cos[cos_idx].users[username].password = sha256(new_password)
     return true
 end
